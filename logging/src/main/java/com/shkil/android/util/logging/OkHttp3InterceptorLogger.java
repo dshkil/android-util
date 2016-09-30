c7ef6497c69fd10da40630089a338238c404a313
Dmitry Shkil <dshkil@cogniance.com>
2016-09-30 12:17:34 +0300

    Allow specify log level in OkHttp3InterceptorLogger

--- logging/src/main/java/com/shkil/android/util/logging/OkHttp3InterceptorLogger.java
+++ logging/src/main/java/com/shkil/android/util/logging/OkHttp3InterceptorLogger.java
@@ -1,18 +1,48 @@
 package com.shkil.android.util.logging;
 
+import android.util.Log;
+
 import okhttp3.logging.HttpLoggingInterceptor;
 
 public class OkHttp3InterceptorLogger implements HttpLoggingInterceptor.Logger {
 
     private final Logger logger;
+    private final int level;
 
     public OkHttp3InterceptorLogger(Logger logger) {
+        this(logger, Log.DEBUG);
+    }
+
+    public OkHttp3InterceptorLogger(Logger logger, int level) {
+        if (logger == null) {
+            throw new NullPointerException("logger");
+        }
+        if (level < Log.VERBOSE || level > Log.ERROR) {
+            throw new IllegalArgumentException("level");
+        }
         this.logger = logger;
+        this.level = level;
     }
 
     @Override
     public void log(String message) {
-        logger.debug(message);
+        switch (level) {
+            case Log.VERBOSE:
+                logger.trace(message);
+                break;
+            case Log.DEBUG:
+                logger.debug(message);
+                break;
+            case Log.INFO:
+                logger.info(message);
+                break;
+            case Log.WARN:
+                logger.warn(message);
+                break;
+            case Log.ERROR:
+                logger.error(message);
+                break;
+        }
     }
 
 }
