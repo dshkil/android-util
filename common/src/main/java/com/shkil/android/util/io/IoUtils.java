ff525e9c5c3263afe9c9e9889f5606861b458548
Dmytro Shkil <d.shkil@gmail.com>
2017-10-06 17:21:32 +0300

    Fix and develop IoUtils

--- common/src/main/java/com/shkil/android/util/io/IoUtils.java
+++ common/src/main/java/com/shkil/android/util/io/IoUtils.java
@@ -2,9 +2,13 @@ package com.shkil.android.util.io;
 
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.WillClose;
 
@@ -25,7 +29,11 @@ public class IoUtils {
         }
     }
 
-    public static byte[] parcelableToBytes(Parcelable object) {
+    @Nullable
+    public static byte[] marshallParcelable(@Nullable Parcelable object) {
+        if (object == null) {
+            return null;
+        }
         Parcel parcel = Parcel.obtain();
         try {
             object.writeToParcel(parcel, 0);
@@ -35,9 +43,44 @@ public class IoUtils {
         }
     }
 
-    public static <V extends Parcelable> V createParcelable(byte[] data, Parcelable.Creator<V> creator) {
+    @Nullable
+    public static <V extends Parcelable> V unmarshallParcelable(@Nullable byte[] data, Parcelable.Creator<V> creator) {
+        if (data == null) {
+            return null;
+        }
         Parcel parcel = Parcel.obtain();
-        parcel.unmarshall(data, 0, data.length);
-        return creator.createFromParcel(parcel);
+        try {
+            parcel.unmarshall(data, 0, data.length);
+            parcel.setDataPosition(0);
+            return creator.createFromParcel(parcel);
+        } finally {
+            parcel.recycle();
+        }
+    }
+
+    @NonNull
+    public static byte[] marshallParcelableTypedList(@Nullable List<? extends Parcelable> list) {
+        Parcel parcel = Parcel.obtain();
+        try {
+            parcel.writeTypedList(list);
+            return parcel.marshall();
+        } finally {
+            parcel.recycle();
+        }
+    }
+
+    @Nullable
+    public static <V> ArrayList<V> unmarshallParcelableTypedList(@Nullable byte[] data, Parcelable.Creator<V> creator) {
+        if (data == null) {
+            return null;
+        }
+        Parcel parcel = Parcel.obtain();
+        try {
+            parcel.unmarshall(data, 0, data.length);
+            parcel.setDataPosition(0);
+            return parcel.createTypedArrayList(creator);
+        } finally {
+            parcel.recycle();
+        }
     }
 }
